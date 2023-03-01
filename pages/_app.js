04675c1f8a90e962eb10d92c4c4bbfb4d8045fc7
Aleksey Solovyev <solovyev.a@icloud.com>
2023-03-01 19:41:08 +0300

    add dev lines

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,22 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
-  const getLayout = Component.getLayout ?? ((page)=>page);
-  return <>{getLayout(<Component {...pageProps} />)}</>
+  const getLayout = Component.getLayout ?? ((page) => page);
+
+  useEffect(() => {
+    if (typeof window == "undefined") return;
+
+    const handler = () => document.body.classList.toggle("dev");
+
+    window.addEventListener("click", handler);
+
+    return () => {
+      window.removeEventListener("click", handler);
+    };
+  }, []);
+  return <>{getLayout(<Component {...pageProps} />)}</>;
 }
 
-export default MyApp
+export default MyApp;
